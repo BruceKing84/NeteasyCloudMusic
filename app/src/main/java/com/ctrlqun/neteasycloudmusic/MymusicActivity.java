62be45b6fa1f044ba0c80df0a85fb8530072805b
BruceKing84 <zhiwenzhang@ctrlqun.com>
2018-03-30 17:05:23 +0800

    version 1.0: add bottom bar

--- app/src/main/java/com/ctrlqun/neteasycloudmusic/MymusicActivity.java
+++ app/src/main/java/com/ctrlqun/neteasycloudmusic/MymusicActivity.java
@@ -3,11 +3,14 @@ package com.ctrlqun.neteasycloudmusic;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MymusicActivity extends AppCompatActivity {
+import com.ctrlqun.view.BottomBarView;
+
+public class MymusicActivity extends BottomBarListenerActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mymusic);
+        ((BottomBarView) findViewById(R.id.id_BottomBarView)).setOnBottomBarListener(this);
     }
 }
